c8692e98ef84aa1c0632c64030d45d41155a7757
Tyler M LeFevre <46289255+TLeFevre2003@users.noreply.github.com>
2024-02-23 20:33:45 -0700

    Initial Commit

---
+++ My Notes/My_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_NotesApp.swift
+//  My Notes
+//
+//  Created by Tyler LeFevre on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct My_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
